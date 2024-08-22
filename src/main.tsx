6ba7b865b724abe1825f87c207b5a6ee38393599
vadiszzz <vadim.spb.korobey@gmail.com>
2024-08-22 19:35:51 +0400

    Workflow

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+// this manifest is used temporarily for development purposes
+const manifestUrl = "https://vadiszzz.github.io/ton_web_demo/tonconnect-manifest.json"
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+)
